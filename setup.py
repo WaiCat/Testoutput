e023e488799c5e210ff44cab69ff622f153ffce0
WaiCat <98246479+WaiCat@users.noreply.github.com>
2022-05-04 10:50:25 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  inclide_package_data = True,
+  name = 'wai_output',
+  version = '0.0.1',
+  description = 'oss_dev_wai_output',
+  author = 'KimTaeHee'
+  auhjor_dmail = 'rxg1123@gmail.com'
+  url = 'https://github.com/WaiCat/Testoutput/'
+  install_description = ['pytest']
+  long_description = 'oss_dev_wai_output python module',
+  long_description_content_type = 'text/markdown', 
+  classifiers=[
+    "Prigramming Lanuage :: Python :: 3",
+    "Operatiog System :: OS Independent",
+  ],
+)
+  
